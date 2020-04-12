860be0b6d4822243ac0dc714bf8e8c9fdaffa405
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2020-04-12 22:46:18 +0200

    :sparkles: introduce footer

---
+++ lib/components/footer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Footer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Made with ❤️ by Taiyr Begeyev',
+      ),
+    );
+  }
+}
