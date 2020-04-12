eab9e695f7ee08e48af38fb6e744feebfd9f2875
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2020-04-12 17:14:27 +0200

    :sparkles: add app file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:identifia/main.dart';
+import 'package:identifia/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Identifia());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
